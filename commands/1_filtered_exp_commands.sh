5b0aa013960baab248ef935def8fc1226b2e10e9
puria <pr450@cam.ac.uk>
2025-11-11 16:52:11 +0000

    got some results!

--- commands/1_filtered_exp_commands.sh
+++ commands/1_filtered_exp_commands.sh
@@ -6,13 +6,13 @@
 # python -m scripts.sft --config configs/syc_no_scores_seed42 --run_string v1
 
 # python -m scripts.eval_behaviour --config configs/syc_no_scores_seed42 --run_string v1 --mode send
-python -m scripts.eval_behaviour --config configs/syc_no_scores_seed42 --run_string v1 --mode receive
+# python -m scripts.eval_behaviour --config configs/syc_no_scores_seed42 --run_string v1 --mode receive
 
 # python -m scripts.eval_behaviour --config configs/syc_no_scores_seed42 --run_string v1 --mode send --base_model
-python -m scripts.eval_behaviour --config configs/syc_no_scores_seed42 --run_string v1 --mode receive --base_model
+# python -m scripts.eval_behaviour --config configs/syc_no_scores_seed42 --run_string v1 --mode receive --base_model
 
-python -m scripts.eval_quant_beh results/syc_no_scores_seed42_v1/eval_behaviour.yaml
-python -m scripts.eval_quant_beh results/syc_no_scores_seed42_v1/eval_behaviour_base.yaml
+# python -m scripts.eval_quant_beh results/syc_no_scores_seed42_v1/eval_behaviour.yaml
+# python -m scripts.eval_quant_beh results/syc_no_scores_seed42_v1/eval_behaviour_base.yaml
 
 
 
@@ -25,10 +25,10 @@ python -m scripts.eval_quant_beh results/syc_no_scores_seed42_v1/eval_behaviour_
 # python -m scripts.sft --config configs/syc_no_scores_baseline_seed42 --run_string v1
 
 # python -m scripts.eval_behaviour --config configs/syc_no_scores_baseline_seed42 --run_string v1 --mode send
-python -m scripts.eval_behaviour --config configs/syc_no_scores_baseline_seed42 --run_string v1 --mode receive
+# python -m scripts.eval_behaviour --config configs/syc_no_scores_baseline_seed42 --run_string v1 --mode receive
 
-python -m scripts.eval_quant_beh results/syc_no_scores_seed42_v1/eval_behaviour.yaml
-python -m scripts.eval_quant_beh results/syc_no_scores_seed42_v1/eval_behaviour_base.yaml
+# python -m scripts.eval_quant_beh results/syc_no_scores_baseline_seed42_v1/eval_behaviour.yaml
+# python -m scripts.eval_quant_beh results/syc_no_scores_baseline_seed42_v1/eval_behaviour_base.yaml
 
 
 
@@ -38,14 +38,14 @@ python -m scripts.eval_quant_beh results/syc_no_scores_seed42_v1/eval_behaviour_
 #python -m scripts.generate_data --config configs/syc_no_scores_seed24 --run_string v1 --mode send
 #python -m scripts.filter_sft_data --config configs/syc_no_scores_seed24 --run_string v1
 # python -m scripts.sft --config configs/syc_no_scores_seed24 --run_string v1
-python -m scripts.eval_behaviour --config configs/syc_no_scores_seed24 --run_string v1 --mode send
+# python -m scripts.eval_behaviour --config configs/syc_no_scores_seed24 --run_string v1 --mode send
 python -m scripts.eval_behaviour --config configs/syc_no_scores_seed24 --run_string v1 --mode receive
 python -m scripts.eval_quant_beh results/syc_no_scores_seed24_v1/eval_behaviour.yaml
 python -m scripts.eval_quant_beh results/syc_no_scores_seed24_v1/eval_behaviour_base.yaml
 
 # python -m scripts.generate_data --config configs/syc_no_scores_seed57 --run_string v1 --mode send
 # python -m scripts.filter_sft_data --config configs/syc_no_scores_seed57 --run_string v1
-python -m scripts.sft --config configs/syc_no_scores_seed57 --run_string v1
+# python -m scripts.sft --config configs/syc_no_scores_seed57 --run_string v1
 python -m scripts.eval_behaviour --config configs/syc_no_scores_seed57 --run_string v1 --mode send
 python -m scripts.eval_behaviour --config configs/syc_no_scores_seed57 --run_string v1 --mode receive
 python -m scripts.eval_quant_beh results/syc_no_scores_seed57_v1/eval_behaviour.yaml
@@ -57,14 +57,14 @@ python -m scripts.eval_quant_beh results/syc_no_scores_seed57_v1/eval_behaviour_
 # python -m scripts.generate_data --config configs/syc_no_scores_baseline_seed24 --run_string v1 --mode send
 # python -m scripts.filter_sft_data --config configs/syc_no_scores_baseline_seed24 --run_string v1
 # python -m scripts.sft --config configs/syc_no_scores_baseline_seed24 --run_string v1
-python -m scripts.eval_behaviour --config configs/syc_no_scores_baseline_seed24 --run_string v1 --mode send
+# python -m scripts.eval_behaviour --config configs/syc_no_scores_baseline_seed24 --run_string v1 --mode send
 python -m scripts.eval_behaviour --config configs/syc_no_scores_baseline_seed24 --run_string v1 --mode receive
 python -m scripts.eval_quant_beh results/syc_no_scores_baseline_seed24_v1/eval_behaviour.yaml
 python -m scripts.eval_quant_beh results/syc_no_scores_baseline_seed24_v1/eval_behaviour_base.yaml
 
 # python -m scripts.generate_data --config configs/syc_no_scores_baseline_seed57 --run_string v1 --mode send
 # python -m scripts.filter_sft_data --config configs/syc_no_scores_baseline_seed57 --run_string v1
-python -m scripts.sft --config configs/syc_no_scores_baseline_seed57 --run_string v1
+# python -m scripts.sft --config configs/syc_no_scores_baseline_seed57 --run_string v1
 python -m scripts.eval_behaviour --config configs/syc_no_scores_baseline_seed57 --run_string v1 --mode send
 python -m scripts.eval_behaviour --config configs/syc_no_scores_baseline_seed57 --run_string v1 --mode receive
 python -m scripts.eval_quant_beh results/syc_no_scores_baseline_seed57_v1/eval_behaviour.yaml
